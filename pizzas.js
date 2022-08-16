0618bbb024f8311db4dcc84df8690b1380bbfe1f
Joao Torquetti <joao.torquetti@outlook.ie>
2022-08-16 22:28:06 +0200

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,21 @@
+let pizzaJson = [
+    {id:1, name:'Margherita', img:'images/pizza.png', price:11.00, sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, mozzarella, extra mozzarella & oregano'},
+    {id:2, name:'Peperoni', img:'images/pizza2.png', price:11.50, sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, mozzarella & pepperoni'},
+    {id:3, name:'Ham', img:'images/pizza3.png', price: 11.50, sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, Mozzarella, Ham'},
+    {id:4, name:'Hawaiian', img:'images/pizza4.png', price:12.00, sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, mozzarella, grilled ham, extra mozzarella & pineapple'},
+    {id:5, name:'4 Cheese', img:'images/pizza5.png', price:12.00, sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, mozzarella, gorgonzola, emmental & goat cheese'},
+    {id:6, name:'Cannibale', img:'images/pizza6.png', price:12.50, sizes:['25cm', '30cm', '35cm'], description:'BBQ sauce, mozzarella, ground beef, merguez, chicken & a swirl of BBQ Sauce'},
+    {id:7, name:'4 Stagioni', img:'images/pizza7.png', price:12.50, sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, mozzarella, pepperoni, ham, peppers & mushrooms'},
+    {id:8, name:'BBQ Chicken & Bacon', img:'images/pizza8.png', price:12.50, sizes:['25cm', '30cm', '35cm'], description:'BBQ sauce, mozzarella, grilled chicken and bacon.'},
+    {id:8, name:'Pizza Extravaganzza', img:'images/pizza9.png', price:12.50, sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, mozzarella, pepperoni, grilled ham, mushrooms, onion, paprika, ground beef & olives'}
+];
+
+/*{id:1, name:'Margherita', img:'images/pizza.png', price:[8.00, 9.50, 11.00], sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, mozzarella, extra mozzarella & oregano'},
+    {id:2, name:'Peperoni', img:'images/pizza2.png', price:[8.50, 10.00, 11.50], sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, mozzarella & pepperoni'},
+    {id:3, name:'Ham', img:'images/pizza3.png', price: [8.50, 10.00, 11.50], sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, Mozzarella, Ham'},
+    {id:4, name:'Hawaiian', img:'images/pizza4.png', price:[9.00, 10.50, 12.00], sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, mozzarella, grilled ham, extra mozzarella & pineapple'},
+    {id:5, name:'4 Cheese', img:'images/pizza5.png', price:[9.00, 10.50, 12.00], sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, mozzarella, gorgonzola, emmental & goat cheese'},
+    {id:6, name:'Cannibale', img:'images/pizza6.png', price:[9.50, 11.00, 12.50], sizes:['25cm', '30cm', '35cm'], description:'BBQ sauce, mozzarella, ground beef, merguez, chicken & a swirl of BBQ Sauce'},
+    {id:7, name:'4 Stagioni', img:'images/pizza7.png', price:[9.50, 11.00, 12.50], sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, mozzarella, pepperoni, ham, peppers & mushrooms'},
+    {id:8, name:'BBQ Chicken & Bacon', img:'images/pizza8.png', price:[9.50, 11.00, 12.50], sizes:['25cm', '30cm', '35cm'], description:'BBQ sauce, mozzarella, grilled chicken and bacon.'},
+    {id:8, name:'Pizza Extravaganzza', img:'images/pizza9.png', price:[9.50, 11.00, 12.50], sizes:['25cm', '30cm', '35cm'], description:'Tomato sauce, mozzarella, pepperoni, grilled ham, mushrooms, onion, paprika, ground beef & olives'} */
